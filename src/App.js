ccfa241e7b7fb1203f98dd058fb2e0c941654f90
prakrutivaghasiya <prakrutivaghasiya1@gmail.com>
2024-06-10 19:04:41 -0300

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import HomePage from "./components/HomePage";
+import BookingPage from './components/BookingPage';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<HomePage />} />
+            <Route path='/booking' element={<BookingPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
